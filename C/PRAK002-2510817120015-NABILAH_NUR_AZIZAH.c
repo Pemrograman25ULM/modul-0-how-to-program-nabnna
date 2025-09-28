5dbfabb79a35bfdeeac9bd45d2eeebfbbf4e7ac9
nabnna <nabnna837@gmail.com>
2025-09-28 18:12:35 +0800

    Add files via upload

---
+++ C/PRAK002-2510817120015-NABILAH_NUR_AZIZAH.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+    printf("Selamat Pagi, Nabilah Nur Azizah\n");
+    printf("Selamat Siang, Nabilah Nur Azizah\n");
+    printf("Selamat Malam. Nabilah Nur Azizah");
+    return 0;
+}
---
+++ C/PRAK004-2510817120015-NABILAH_NUR_AZIZAH.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    printf("################################\n");
+    printf("#                              #\n");
+    printf("#      Nabilah Nur Azizah      #\n");
+    printf("#        2510817120015         #\n");
+    printf("#                              #\n");
+    printf("################################\n");
+    return 0;
+}
